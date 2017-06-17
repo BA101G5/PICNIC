b789a16171df6aa9d3418c9040e4b34c30fd5b34
nocama <l978645312@gmail.com>
2017-06-17 19:20:23 +0800

    goods_rentjdbc picmemjdbc

--- BA101G5/src/com/goods_rent/model/Goods_RentJDBCDAO.java
+++ BA101G5/src/com/goods_rent/model/Goods_RentJDBCDAO.java
@@ -3,16 +3,16 @@ package com.goods_rent.model;
 import java.util.List;
 
 public class Goods_RentJDBCDAO implements Goods_Rent_interface{
-		String driver ="Oracle.driver.jdbc.OracleDriver";
+		String driver ="Oracle.jdbc.driver.Oracledriver";
 		String url ="jdbc:oracle:thin@localhost:1521:XE";
 		String userid ="ba101";
 		String passwd ="ba101";
 	
-		private static final String INSERT_STMT ="";
-		private static final String GET_ALL_STMT ="";
-		private static final String GET_ONE_STMT ="";
-		private static final String DELETE_STMT ="";
-		private static final String UPDATE_STMT ="";
+		private static final String INSERT_STMT ="insert into GOODS_RENT(GR_NO,MF_NO,P_NO,GR_NAME,GR_DATE,GR_PRICE,GR_COUNT,GR_INFO,GR_IMG,GR_UNTIL,GR_STA) values(?,?,?,?,?,?,?,?,?,?,?)";
+		private static final String GET_ALL_STMT ="select * from GOODS_RENT order by GR_NO";
+		private static final String GET_ONE_STMT ="select GR_NO,MF_NO,P_NO,GR_NAME,GR_DATE,GR_PRICE,GR_COUNT,GR_INFO,GR_IMG,GR_UNTIL,GR_STA from GOODS_RENT where GR_NO =?";
+		private static final String DELETE_STMT ="delete from GOODS_RENT where GR_NO =?";
+		private static final String UPDATE_STMT ="update GOODS_RENT set MF_NO=?,P_NO=?,GR_NAME=?,GR_DATE=?,GR_PRICE=?,GR_COUNT=?,GR_INFO=?,GR_IMG=?,GR_UNTIL=?,GR_STA=? where GR_NO=?";
 
 	@Override
 	public void insert(Goods_RentVO goods_rentVO) {
