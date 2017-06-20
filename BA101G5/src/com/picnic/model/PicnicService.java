c77e65ea81e502282a18801d837d4ada2bea0fec
nocama <l978645312@gmail.com>
2017-06-20 13:40:49 +0800

    new version 17-06-20

---
+++ BA101G5/src/com/picnic/model/PicnicService.java
@@ -0,0 +1,32 @@
+package com.picnic.model;
+
+import java.util.List;
+
+import com.place.model.PlaceVO;
+
+public class PicnicService {
+	private PicnicDAO dao = null;
+
+	public PicnicService() {
+		dao = new PicnicDAO();
+	}
+
+	public PicnicVO addPicnic() {
+		return null;
+	}
+
+	public PicnicVO updatePicnic() {
+		return null;
+	}
+
+	public void deletePicnic() {
+	}
+
+	public PicnicVO getOne() {
+		return null;
+	}
+
+	public List<PicnicVO> getAll() {
+		return null;
+	}
+}
---
+++ BA101G5/src/com/place/model/PlaceService.java
@@ -0,0 +1,31 @@
+package com.place.model;
+
+import java.util.List;
+
+import com.goods_sell.model.Goods_SellVO;
+
+public class PlaceService {
+	private PlaceDAO dao= null;
+	public PlaceService(){
+		dao = new PlaceDAO();
+	}
+	
+	public PlaceVO addPlace() {
+		return null;
+	}
+
+	public PlaceVO updatePlace() {
+		return null;
+	}
+
+	public void deletePlace() {
+	}
+
+	public PlaceVO getOne() {
+		return null;
+	}
+
+	public List<PlaceVO> getAll() {
+		return null;
+	}
+}
