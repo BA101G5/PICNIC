93ae66c0e793590328460c21e2afd67627b71f38
nocama <l978645312@gmail.com>
2017-07-08 06:06:07 +0800

    new version

--- BA101G5/src/com/picnic/controller/PicnicServlet.java
+++ BA101G5/src/com/picnic/controller/PicnicServlet.java
@@ -1,22 +1,14 @@
 package com.picnic.controller;
-
 import java.io.IOException;
-import java.sql.Date;
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import com.picmem.model.PicmemService;
 import com.picnic.model.PicnicService;
 import com.place.model.PlaceService;
-
-import net.sf.json.JSONObject;
-
 import javax.servlet.ServletException;
 import java.util.Map;
 import java.util.LinkedHashMap;
@@ -31,6 +23,7 @@ public class PicnicServlet extends HttpServlet {
 		HttpSession session = req.getSession();
 		req.setCharacterEncoding("UTF-8");
 		String action = req.getParameter("action");
+		System.out.println(action);
 
 		if (action.equals("checkbeforeinsert")) {
 			Map<String, String> errorMsgs = new LinkedHashMap<String, String>();
@@ -45,17 +38,18 @@ public class PicnicServlet extends HttpServlet {
 				}
 				String address = req.getParameter("address");
 				String area = req.getParameter("area");
-				String tladdress = area + address;
-				String addressReg = "^[(\u4e00-\u9fa5)(0-9_)]{6,15}$";
+
+				String addressReg = "^[(\u4e00-\u9fa5)(0-9_)]{6,30}$";
 				if (address == null || address.trim().length() == 0) {
+
 					errorMsgs.put("address", "地址不能為空");
 				} else if (!address.trim().matches(addressReg)) {
+					System.out.println("-------------------------------------------------");
 					errorMsgs.put("address", "地址必須文中文和數字 且在6~15字之間");
+				} else if ((Integer.getInteger(address) != null)||address.substring(1,2).equals("[^0-9]")) {
+					errorMsgs.put("address", "請輸入正確地址");
 				}
-			
-
-			
-
+				
 				Timestamp picnic_date = null;
 				String date = null;
 				try {
@@ -75,6 +69,15 @@ public class PicnicServlet extends HttpServlet {
 				} catch (Exception e) {
 					errorMsgs.put("people", "請輸入數字");
 				}
+				
+				
+				String tladdress=null;
+				if (address.substring(3,4).equals("市")) {
+					 tladdress = address;
+				} else {
+					 tladdress = area + address;
+				}
+				
 				session.setAttribute("picnic_name", picnic_name);
 				session.setAttribute("area", area);
 				session.setAttribute("tladdress", tladdress);
@@ -103,7 +106,7 @@ public class PicnicServlet extends HttpServlet {
 		if (action.equals("insert")) {
 			Map<String, String> errorMsgs = new LinkedHashMap<String, String>();
 			req.setAttribute("errorMsgs", errorMsgs);
-
+			
 			try {/*************** 移除不必要的資訊 ***************/
 				session.removeAttribute("area");
 				session.removeAttribute("address");
@@ -117,20 +120,23 @@ public class PicnicServlet extends HttpServlet {
 				Integer picnic_pl = (Integer) session.getAttribute("people");
 
 				if (action.equals("insert")) {
+					
 					PicnicService picnicSvc = new PicnicService();
 					String picnic_no = picnicSvc.addPicnic(picnic_name, picnic_date, picnic_pl);
 					PicmemService picmemSvc = new PicmemService();
 					picmemSvc.addowner(picnic_no, account);
+					
 					PlaceService placeSvc = new PlaceService();
-
+					placeSvc.insertplace(account, tladdress, picnic_no);
+					
 				}
-				String url=null;
-			    if(action.equals("insert")){
-			    	url="/picnic/maosecondui3.jsp";
-			    }
-			    
-			    javax.servlet.RequestDispatcher SuccessView =req.getRequestDispatcher(url);
-			    SuccessView.forward(req, res);
+				String url = null;
+				if (action.equals("insert")) {
+					url = "/picnic/maosecondui3.jsp";
+				}
+
+				javax.servlet.RequestDispatcher SuccessView = req.getRequestDispatcher(url);
+				SuccessView.forward(req, res);
 			} catch (Exception e) {
 				errorMsgs.put("Exception", e.getMessage());
 
