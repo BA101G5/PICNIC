71236f528364a1f40243796cb07edaedc5504ef8
h2208339 <h2208339@gmail.com>
2017-07-14 10:50:40 +0800

    xml update

--- BA101G5/src/com/general_member/controller/DBGift.java
+++ BA101G5/src/com/general_member/controller/DBGift.java
@@ -67,7 +67,7 @@ public class DBGift extends HttpServlet {
 	public void init() throws ServletException {
 		try {
 			Context ctx = new javax.naming.InitialContext();
-			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/TestDB3");
+			DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/ba101_5");
 			con = ds.getConnection();
 		} catch (NamingException e) {
 			// TODO Auto-generated catch block
