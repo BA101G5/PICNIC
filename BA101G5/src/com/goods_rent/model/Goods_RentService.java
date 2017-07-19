cdfa3ff1a814a2ec17835c2940afbdb4f16b01f2
kaoru <kaoru@kaoru-PC>
2017-07-19 10:01:06 +0800

    goods_rent update

--- BA101G5/src/com/goods_rent/model/Goods_RentService.java
+++ BA101G5/src/com/goods_rent/model/Goods_RentService.java
@@ -31,4 +31,23 @@ public class Goods_RentService {
 	public List<Goods_RentVO> findbyplace(String mf_no, String place) {
 		return dao.findbyplace(mf_no, place);
 	}
+	//-------------------------------------------------------------------------
+	public Goods_RentVO k_addGoods_Rent() {
+		return null;
+	}
+
+	public Goods_RentVO k_updateGoods_Rent() {
+		return null;
+	}
+
+	public void k_deleteGoods_Rent() {
+	}
+
+	public Goods_RentVO k_getOne(String gr_no) {
+		return dao.k_findByPrimaryKey(gr_no);
+	}
+
+	public List<Goods_RentVO> k_getAll() {
+		return dao.k_getAll();
+	}
 }
