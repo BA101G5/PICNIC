2a051ee081ce8f281e777d2424d2579e6922cab5
kaoru <kaoru@kaoru-PC>
2017-07-19 10:48:04 +0800

    picmem update

--- BA101G5/src/com/picmem/model/PicmemJNDIDAO.java
+++ BA101G5/src/com/picmem/model/PicmemJNDIDAO.java
@@ -27,8 +27,8 @@ public class PicmemJNDIDAO implements PicmemDAO_interface {
 	private static final String GET_ONE_STMT = "select PICNIC_NO,MEM_NO,PICMEM_IDEN,PICMEM_STA,MEM_LONGI,MEM_LATIT from PICMEM where PICNIC_NO =? and MEM_NO =? order by PICNIC_NO";
 	private static final String DELETE_STMT = "delete from PICMEM where PICNIC_NO =? MEM_NO =?";
 	private static final String UPDATE_STMT = "update PICMEM set PICMEM_IDEN = ?,PICMEM_STA =?,MEM_LONGI =?,MEM_LATIT =? where PICNIC_NO =? and MEM_NO =?";
-	private static final String INSERT_OWNER_STMT = "insert into PICMEM(PICNIC_NO,MEM_NO,PICMEM_IDEN)values(?,?,'��蜓')";
-	private static final String GET_BYMEMNO_STMT = "select PICNIC_NO from PICMEM where MEM_NO =? and PICMEM_IDEN=\'��蜓\'";
+	private static final String INSERT_OWNER_STMT = "insert into PICMEM(PICNIC_NO,MEM_NO,PICMEM_IDEN)values(?,?,'A')";
+	private static final String GET_BYMEMNO_STMT = "select PICNIC_NO from PICMEM where MEM_NO =? and PICMEM_IDEN=\'A\'";
 	@Override
 	public void insert(PicmemVO picmemVO) {
 		Connection con = null;
@@ -318,5 +318,299 @@ public class PicmemJNDIDAO implements PicmemDAO_interface {
 
 		return list;
 	}
+//-----------------------------------------------------------------------------------------
+	private static final String K_INSERT_STMT = "insert into PICMEM (PICNIC_NO,MEM_NO,PICMEM_IDEN,PICMEM_STA,MEM_LONGI,MEM_LATIT) values(?,?,?,?,?,?)";
+	private static final String K_GET_ALL_STMT = "select * from PICMEM order by PICNIC_NO";
+	private static final String K_GET_ONE_STMT = "select PICNIC_NO,MEM_NO,PICMEM_IDEN,PICMEM_STA,MEM_LONGI,MEM_LATIT from PICMEM where PICNIC_NO =? MEM_NO =? order by PICNIC_NO";
+	private static final String K_DELETE_STMT = "delete from PICMEM where PICNIC_NO =? and MEM_NO =?";
+	private static final String K_UPDATE_STMT = "update PICMEM set PICMEM_IDEN = ?,PICMEM_STA =?,MEM_LONGI =?,MEM_LATIT =? where PICNIC_NO =? and MEM_NO =?";
+	private static final String K_INSERT_OWNER_STMT = "insert into PICMEM(PICNIC_NO,MEM_NO,PICMEM_IDEN)values(?,?,'�ΥD')";
+	private static final String K_GET_ONE_STMT_ALL_LIST = "select * from PICMEM where MEM_NO =? order by PICNIC_NO";
+	@Override
+	public void k_insert(PicmemVO picmemVO) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
 
+		try {
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(K_INSERT_STMT);
+			pstmt.setString(1, picmemVO.getPicnic_no());
+			pstmt.setString(2, picmemVO.getMem_no());
+			pstmt.setString(3, picmemVO.getPicmem_iden());
+			pstmt.setString(4, picmemVO.getPicmem_sta());
+			pstmt.setDouble(5, picmemVO.getMem_longi());
+			pstmt.setDouble(6, picmemVO.getMem_latit());
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new RuntimeException("A database error occured. " + e.getMessage());
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public void k_update(PicmemVO picmemVO) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(K_UPDATE_STMT);
+
+			pstmt.setString(1, picmemVO.getPicmem_iden());
+			pstmt.setString(2, picmemVO.getPicmem_sta());
+			pstmt.setDouble(3, picmemVO.getMem_longi());
+			pstmt.setDouble(4, picmemVO.getMem_latit());
+			pstmt.setString(5, picmemVO.getPicnic_no());
+			pstmt.setString(6, picmemVO.getMem_no());
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new RuntimeException("A database error occured. " + e.getMessage());
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void k_delete(String picnic_no, String mem_no) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(K_DELETE_STMT);
+			pstmt.setString(1, picnic_no);
+			pstmt.setString(2, mem_no);
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new RuntimeException("A database error occured. " + e.getMessage());
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+	}
+
+	@Override
+	public PicmemVO k_findByPrimaryKey(String picnic_no, String mem_no) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		PicmemVO picmemVO = null;
+		ResultSet rs = null;
+
+		try {
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(K_GET_ONE_STMT);
+			pstmt.setString(1, picnic_no);
+			pstmt.setString(2, mem_no);
+			rs = pstmt.executeQuery();
+
+			rs.next();
+			picmemVO = new PicmemVO();
+			picmemVO.setPicnic_no(rs.getString("PICNIC_NO"));
+			picmemVO.setMem_no(rs.getString("MEM_NO"));
+			picmemVO.setPicmem_iden(rs.getString("PICMEM_IDEN"));
+			picmemVO.setPicmem_sta(rs.getString("PICMEM_STA"));
+			picmemVO.setMem_longi(rs.getDouble("MEM_LONGI"));
+			picmemVO.setMem_latit(rs.getDouble("MEM_LATIT"));
+
+		} catch (SQLException e) {
+			throw new RuntimeException("A database error occured. " + e.getMessage());
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return picmemVO;
+	}
+
+	@Override
+	public List<PicmemVO> k_getAll() {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		List<PicmemVO> list = new ArrayList<PicmemVO>();
+		ResultSet rs = null;
+
+		try {
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(K_GET_ALL_STMT);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				PicmemVO picmemVO = new PicmemVO();
+				picmemVO.setPicnic_no(rs.getString("PICNIC_NO"));
+				picmemVO.setMem_no(rs.getString("MEM_NO"));
+				picmemVO.setPicmem_iden(rs.getString("PICMEM_IDEN"));
+				picmemVO.setPicmem_sta(rs.getString("PICMEM_STA"));
+				picmemVO.setMem_longi(rs.getDouble("MEM_LONGI"));
+				picmemVO.setMem_latit(rs.getDouble("MEM_LATIT"));
+				list.add(picmemVO);
+			}
+
+		} catch (SQLException e) {
+			throw new RuntimeException("A database error occured. " + e.getMessage());
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return list;
+
+	}
+	@Override
+	public void k_insertowner(PicmemVO picmemVO) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			con=ds.getConnection();
+			pstmt = con.prepareStatement(K_INSERT_OWNER_STMT);
+			pstmt.setString(1, picmemVO.getPicnic_no());
+			pstmt.setString(2, picmemVO.getMem_no());
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new RuntimeException("A database error occured. " + e.getMessage());
+		} finally {
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+	}
+	//----
+	public List<String> k_getMemPicList(String mem_no) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<String> memPicList = new ArrayList<String>();
+
+		try {
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(K_GET_ONE_STMT_ALL_LIST);
+			pstmt.setString(1, mem_no);
+			rs = pstmt.executeQuery();
+
+			while(rs.next()){
+				memPicList.add(rs.getString("PICNIC_NO"));
+			}
+
+		} catch (SQLException e) {
+			throw new RuntimeException("A database error occured. " + e.getMessage());
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return memPicList;
+	}
 }
