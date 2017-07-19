e4adc94b1729093a2008057b7fd8a1315ad34e90
kaoru <kaoru@kaoru-PC>
2017-07-19 10:12:26 +0800

    goods_sell update

--- BA101G5/src/com/goods_sell/model/Goods_SellDAO_interface.java
+++ BA101G5/src/com/goods_sell/model/Goods_SellDAO_interface.java
@@ -7,5 +7,10 @@ public interface Goods_SellDAO_interface {
 		public void delete(String gs_no);
 		public Goods_SellVO findByPrimaryKey(String gs_no);
 		public List<Goods_SellVO> getAll();
-
+//--------------------------------------------------------------
+		public void k_insert(Goods_SellVO goods_sellVO);
+		public void k_update(Goods_SellVO goods_sellVO);
+		public void k_delete(String gs_no);
+		public Goods_SellVO k_findByPrimaryKey(String gs_no);
+		public List<Goods_SellVO> k_getAll();
 }
--- BA101G5/src/com/goods_sell/model/Goods_SellService.java
+++ BA101G5/src/com/goods_sell/model/Goods_SellService.java
@@ -30,6 +30,31 @@ public class Goods_SellService {
 		
 		return dao.findByPrimaryKey(gs_no);
 	}
+//-----------------------------------------------------------------------
+	public Goods_SellVO k_addGoods_Sell() {
+		return null;
+	}
 
+	public Goods_SellVO k_updateGoods_Sell() {
+		return null;
+	}
+
+	public void k_deleteGoods_Sell() {
+	}
+
+	public Goods_SellVO k_getOne(String gs_no) {
+		//String table="goods_sells";
+		Goods_SellVO goods_sellVO =dao.k_findByPrimaryKey(gs_no);
+		//Imageout img=new Imageout();
+		//System.out.println("hello");
+		//String url=img.imageoutput(contextpath,table, goods_sellVO.getGs_name(), goods_sellVO.getGs_img());
+		//goods_sellVO.setUrl(url);
+		return goods_sellVO;
+	}
+
+	public List<Goods_SellVO> k_getAll() {
+		
+		return dao.k_getAll();
+	}
 
 }
