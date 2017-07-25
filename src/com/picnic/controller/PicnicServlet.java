a6b52697b8be5a0f8be5c762ea0202498ef26d08
shyangs <shyangs@gmail.com>
2017-07-25 19:54:03 +0800

    action 可能 null
    action.equals("checkbeforeinsert") -> "checkbeforeinsert".equals(action)

--- src/com/picnic/controller/PicnicServlet.java
+++ src/com/picnic/controller/PicnicServlet.java
@@ -33,8 +33,8 @@ public class PicnicServlet extends HttpServlet {
 		HttpSession session = req.getSession();
 		req.setCharacterEncoding("UTF-8");
 		String action = req.getParameter("action");
-		System.out.println(action);
-		if (action.equals("checkbeforeinsert")) {
+//		System.out.println(action);
+		if ("checkbeforeinsert".equals(action)) {
 			Map<String, String> errorMsgs = new LinkedHashMap<String, String>();
 			req.setAttribute("errorMsgs", errorMsgs);
 			try {
@@ -84,7 +84,7 @@ public class PicnicServlet extends HttpServlet {
 				}
 
 				String tladdress = null;
-				System.out.println();
+//				System.out.println();
 
 				if (address.contains("縣")||address.contains("市")) {
 					tladdress = address;
@@ -103,7 +103,7 @@ public class PicnicServlet extends HttpServlet {
 				String url = null;
 				if (!errorMsgs.isEmpty()) {
 					url = "/picnic/maosecondui.jsp";
-				} else if (action.equals("checkbeforeinsert")) {
+				} else if ("checkbeforeinsert".equals(action)) {
 					url = "/picnic/maosecondui2.jsp";
 				}
 				javax.servlet.RequestDispatcher successView = req.getRequestDispatcher(url);
@@ -120,7 +120,7 @@ public class PicnicServlet extends HttpServlet {
 		
 		
 
-		if (action.equals("insert")) {
+		if ("insert".equals(action)) {
 
 			Map<String, String> errorMsgs = new LinkedHashMap<String, String>();
 			req.setAttribute("errorMsgs", errorMsgs);
@@ -140,7 +140,7 @@ public class PicnicServlet extends HttpServlet {
 
 				Integer picnic_pl = (Integer) session.getAttribute("people");
 
-				if (action.equals("insert")) {
+				if ("insert".equals(action)) {
 					PicnicService picnicSvc = new PicnicService();
 					String picnic_no = picnicSvc.addPicnic(picnic_name, picnic_date, picnic_pl);
 					PicmemService picmemSvc = new PicmemService();
@@ -151,15 +151,15 @@ public class PicnicServlet extends HttpServlet {
 
 					try {
 						placeVO = placeSvc.getOne(tladdress);
-						System.out.println(placeVO + "hello");
+//						System.out.println(placeVO + "hello");
 
 						if (placeVO.getMf_no() != null) {
-							System.out.println(placeVO.getMf_no());
+//							System.out.println(placeVO.getMf_no());
 							orderde_detailSvc.addPlaceOrderde_Detail(placeVO.getP_price(), placeVO.getP_no(), account,
 									picnic_no, tladdress);
 							Goods_RentService goods_rentSvc = new Goods_RentService();
 							List<Goods_RentVO> list = goods_rentSvc.findbyplace(placeVO.getMf_no(), tladdress);
-							System.out.println(list);
+//							System.out.println(list);
 							session.setAttribute("picnic_no", picnic_no);
 							if (!list.isEmpty()) {
 								session.setAttribute("list", list);
@@ -171,8 +171,8 @@ public class PicnicServlet extends HttpServlet {
 						orderde_detailSvc.addPlaceOrderde_Detail(P_price, p_no, account, picnic_no, tladdress);
 					} finally {
 						String url = null;
-						System.out.println(action);
-						if (action.equals("insert")) {
+//						System.out.println(action);
+						if ("insert".equals(action)) {
 							url = "/picnic/maosecondui3.jsp";
 						}
 						javax.servlet.RequestDispatcher SuccessView = req.getRequestDispatcher(url);
